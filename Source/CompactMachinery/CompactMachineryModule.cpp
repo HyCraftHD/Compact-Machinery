f36ef59ab2d5ef16f84e3607e50bc119603f8522
HyCraftHD <hycrafthd@live.de>
2020-09-06 19:45:18 +0200

    Remove erroring code

--- Source/CompactMachinery/CompactMachineryModule.cpp
+++ Source/CompactMachinery/CompactMachineryModule.cpp
@@ -1,19 +1,19 @@
 #include "CompactMachineryModule.h"
 #include "SML/mod/hooking.h"
 #include "FGGameEngine.h"
-#include "SML/util/Logging.h"
-#include <windows.h>
+//#include "SML/util/Logging.h"
+//#include <windows.h>
 
 void FCompactMachineryModule::StartupModule() {
 
-	MessageBox(NULL, "Hello, World!", "Hi!", MB_OK);
+	//MessageBox(NULL, "Hello, World!", "Hi!", MB_OK);
 
-	SUBSCRIBE_VIRTUAL_FUNCTION(UFGGameEngine, UFGGameEngine::Start, [](auto& scope, UFGGameEngine* self_r) {
-		SML::Logging::info("GAME ENDING STARTED __>_> XDXDXDXDXD LOL");
-		for (int i = 0; i < 100; i++) {
-			SML::Logging::info("HOOK WORKED YAY");
-		}
-	})
+	//SUBSCRIBE_VIRTUAL_FUNCTION(UFGGameEngine, UFGGameEngine::Start, [](auto& scope, UFGGameEngine* self_r) {
+	//	SML::Logging::info("GAME ENDING STARTED __>_> XDXDXDXDXD LOL");
+	//	for (int i = 0; i < 100; i++) {
+	//		SML::Logging::info("HOOK WORKED YAY");
+	//	}
+	//})
 }
 
 IMPLEMENT_GAME_MODULE(FCompactMachineryModule, CompactMachinery);
